120f81e70c64a994adca23341a1bb1eca67521b9
Lily <1368025+Lily418@users.noreply.github.com>
2021-09-10 11:06:56 +0100

    Capture ATC window

--- window_test.py
+++ window_test.py
@@ -1,5 +1,6 @@
 from pywinauto.application import Application
+import pywinauto
 
 
-app = Application(backend="uia").connect(title_re=".*Firefox.*")
-print(app.windows()[0])
+app = Application(backend="uia").connect(title_re=".*Microsoft Flight Simulator.*")
+print(app.window(handle=pywinauto.findwindows.find_window(title="ATC")).capture_as_image().save("atc.png", "PNG"))
